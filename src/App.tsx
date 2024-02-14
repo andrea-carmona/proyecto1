2336242bc95eb32a4ba86c44969d2b190f60172f
Andrea Carmona <andrea@carmonaweb.com>
2024-02-14 17:41:08 -0500

    terminacion del hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import {HomePageLayout} from './templates';
 import Header from './organisms/header';
-
+import './App.css';
 import logo from './assets/img/logo.png';
 
 function App() {
   return (
     <HomePageLayout>
      <Header logo={logo}/>
+     <div className='hero'>
+     </div>
      <div>menu</div>
     </HomePageLayout>
   );
 }
 
 export default App;
+
